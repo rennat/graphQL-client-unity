5a9f2789a321487c3cc0d2509680f8775e7d6e44
Tanner Netterville <tannern@gmail.com>
2020-07-19 16:27:34 -0500

    add reference TokenBearerAuthMiddlware

---
+++ Runtime/Core/RequestAuthMiddlewares/TokenBearerAuthMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.WebSockets;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace GraphQlClient.Core.Auth
+{
+    [CreateAssetMenu(fileName = "Token Bearer Auth", menuName = "GraphQlClient/AuthMiddlewares/Token Bearer Auth")]
+    public class TokenBearerAuthMiddleware : RequestAuthMiddleware
+    {
+        public string token;
+
+        public void SetToken(string value) => token = value;
+
+        public override void Prepare(UnityWebRequest request)
+        {
+            request.SetRequestHeader("Authorization", string.Format("Bearer {0}", token));
+        }
+
+        public override void Prepare(ClientWebSocket socket)
+        {
+            socket.Options.SetRequestHeader("Authorization", string.Format("Bearer {0}", token));
+        }
+    }
+}
